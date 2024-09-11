e6d45562502a9735aaafabf322af842b199d8ed4
Ambre.Leveille <leveilleambre05@gmail.com>
2024-09-11 11:28:49 +0200

    Spring boot init

---
+++ server/src/test/java/com/hoadri/retro/RetroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hoadri.retro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
